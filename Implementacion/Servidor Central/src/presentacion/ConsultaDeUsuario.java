aa18077abe6e91571867b927048a602ec496c9d1
Agustín Recoba <agustin.recoba@fing.edu.uy>
2022-08-18 14:17:09 -0300

    Me quedo con lo principal

--- Implementacion/Servidor Central/src/presentacion/ConsultaDeUsuario.java
+++ Implementacion/Servidor Central/src/presentacion/ConsultaDeUsuario.java
@@ -2,21 +2,10 @@ package presentacion;
 
 import java.awt.EventQueue;
 
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
-import java.awt.BorderLayout;
-import javax.swing.JPanel;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import java.awt.Color;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.SwingConstants;
 
 public class ConsultaDeUsuario extends JInternalFrame {
-	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,44 +27,20 @@ public class ConsultaDeUsuario extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public ConsultaDeUsuario() {
-		setTitle("Registrar Usuario");
-		setBounds(100, 100, 409, 270);
-		getContentPane().setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("NickName:");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel.setBounds(10, 11, 103, 14);
-		getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Nombre:");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1.setBounds(10, 36, 103, 14);
-		getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Apellido:");
-		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1.setBounds(10, 61, 103, 14);
-		getContentPane().add(lblNewLabel_1_1);
-		
-		textField = new JTextField();
-		textField.setBounds(125, 8, 258, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Correo electronico:");
-		lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1_1.setBounds(10, 86, 103, 14);
-		getContentPane().add(lblNewLabel_1_1_1);
-		
-		JLabel lblNewLabel_1_1_1_1 = new JLabel("Fecha nacimiento:");
-		lblNewLabel_1_1_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1_1_1.setBounds(10, 111, 103, 14);
-		getContentPane().add(lblNewLabel_1_1_1_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("Tipo de Usuario:");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_2.setBounds(10, 136, 103, 14);
-		getContentPane().add(lblNewLabel_2);
+        // Se inicializa con el controlador de usuarios
 
-	}
+        
+        // Propiedades del JInternalFrame como dimensión, posición dentro del frame, etc.
+        setResizable(true);
+        setIconifiable(true);
+        setMaximizable(true);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setClosable(true);
+        setTitle("Consultar un Usuario");
+        setBounds(30, 30, 400, 280);
+
+        // En este caso usaremos el Absolute Layout y deberemos indicar
+        // la posición absoluta de todos los componentes
+        getContentPane().setLayout(null);
+    }
 }
