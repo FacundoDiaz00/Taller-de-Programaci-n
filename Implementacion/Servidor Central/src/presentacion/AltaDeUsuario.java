39778e041e7235c5e7dc41dd0918cb1a208c7970
Valentina Alaniz <valentinaalaniz@gmail.com>
2022-08-19 20:01:02 -0300

    Haciendo presentacion

--- Implementacion/Servidor Central/src/presentacion/AltaDeUsuario.java
+++ Implementacion/Servidor Central/src/presentacion/AltaDeUsuario.java
@@ -1,6 +1,7 @@
 package presentacion;
 
 import java.awt.EventQueue;
+import logica.controladores.IControladorUsuario;
 
 import javax.swing.JInternalFrame;
 import java.awt.BorderLayout;
@@ -14,9 +15,17 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 
 public class AltaDeUsuario extends JInternalFrame {
-	private JTextField textField;
+	
+	
+	private JTextField nombre;
+	private JTextField nickname;
+	private JTextField fnacimiento;
+	private JTextField apellido;
+	private JTextField correo;
 
 	/**
 	 * Launch the application.
@@ -37,45 +46,97 @@ public class AltaDeUsuario extends JInternalFrame {
 	/**
 	 * Create the frame.
 	 */
-	public AltaDeUsuario() {
+	public AltaDeUsuario(IControladorUsuario icu) {
 		setTitle("Registrar Usuario");
 		setBounds(100, 100, 409, 270);
 		getContentPane().setLayout(null);
+        setResizable(true);
+        setIconifiable(true);
+        setMaximizable(true);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setClosable(true);
 		
-		JLabel lblNewLabel = new JLabel("NickName:");
+		JLabel lblNewLabel = new JLabel("Nickname:");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel.setBounds(10, 11, 103, 14);
+		lblNewLabel.setBounds(20, 11, 77, 14);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Nombre:");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1.setBounds(10, 36, 103, 14);
+		lblNewLabel_1.setBounds(20, 35, 64, 14);
 		getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Apellido:");
 		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1.setBounds(10, 61, 103, 14);
+		lblNewLabel_1_1.setBounds(21, 61, 63, 14);
 		getContentPane().add(lblNewLabel_1_1);
 		
-		textField = new JTextField();
-		textField.setBounds(125, 8, 258, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		nombre = new JTextField();
+		nombre.setBounds(131, 34, 258, 20);
+		getContentPane().add(nombre);
+		nombre.setColumns(10);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Correo electronico:");
+		JLabel lblNewLabel_1_1_1 = new JLabel("Correo:");
 		lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1_1.setBounds(10, 86, 103, 14);
+		lblNewLabel_1_1_1.setBounds(0, 87, 77, 14);
 		getContentPane().add(lblNewLabel_1_1_1);
 		
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("Fecha nacimiento:");
 		lblNewLabel_1_1_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1_1_1.setBounds(10, 111, 103, 14);
+		lblNewLabel_1_1_1_1.setBounds(0, 110, 137, 14);
 		getContentPane().add(lblNewLabel_1_1_1_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Tipo de Usuario:");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_2.setBounds(10, 136, 103, 14);
+		lblNewLabel_2.setBounds(0, 136, 127, 14);
 		getContentPane().add(lblNewLabel_2);
+		
+		nickname = new JTextField();
+		nickname.setColumns(10);
+		nickname.setBounds(131, 9, 258, 20);
+		getContentPane().add(nickname);
+		
+		fnacimiento = new JTextField();
+		fnacimiento.setColumns(10);
+		fnacimiento.setBounds(155, 109, 234, 20);
+		getContentPane().add(fnacimiento);
+		
+		apellido = new JTextField();
+		apellido.setColumns(10);
+		apellido.setBounds(131, 59, 258, 20);
+		getContentPane().add(apellido);
+		
+		correo = new JTextField();
+		correo.setColumns(10);
+		correo.setBounds(131, 84, 258, 20);
+		getContentPane().add(correo);
+		
+		JComboBox tipoUsuario = new JComboBox();
+		tipoUsuario.setBounds(155, 131, 212, 24);
+		getContentPane().add(tipoUsuario);
+		
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(272, 201, 117, 25);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+                limpiarFormulario();
+                setVisible(false);
+			}
+		});
+		btnCancelar.setBounds(10, 201, 117, 25);
+		getContentPane().add(btnCancelar);
+	    private void limpiarFormulario() {
+	        textFieldNombre.setText("");
+	        textFieldApellido.setText("");
+	        textFieldCI.setText("");
+	    }
 
 	}
 }
