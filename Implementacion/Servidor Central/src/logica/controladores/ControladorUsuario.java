ad21c4c9c64529522236a9427b12b98d01007e56
Valentina Alaniz <valentinaalaniz@gmail.com>
2022-08-19 19:00:22 -0300

    Funciones

--- Implementacion/Servidor Central/src/logica/controladores/ControladorUsuario.java
+++ Implementacion/Servidor Central/src/logica/controladores/ControladorUsuario.java
@@ -1,7 +1,40 @@
 package logica.controladores;
-
+import logica.manejadores.ManejadorUsuario;
+import logica.entidades.Turista;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import logica.entidades.Proveedor;
 /**
  * @author Equipo taller prog 16
  */
 public class ControladorUsuario implements IControladorUsuario {
+    public ArrayList<String> obtenerIdUsuarios() {
+        // TODO implement here
+        return new ArrayList<>();
+    }
+	
+    public Boolean altaTurista(String nickname, String nombre, String apellido, String correo,LocalDate FNacimiento, String nacionalidad){
+        ManejadorUsuario mu = ManejadorUsuario.getInstancia();
+        if(mu.existeUsuario(nickname)) {
+            //throw new UsuarioRepetidoException("El usuario " + ci + " ya esta registrado");
+        	return false;
+        }
+
+        Turista u = new Turista(nickname, nombre, apellido, correo,FNacimiento,nacionalidad);
+        mu.addUsuario(u);
+        return true;
+    }
+    
+    public Boolean altaProveedor(String nickname,String nombre,String apellido,String correo,String descripcion,String link, LocalDate FNacimiento){
+        ManejadorUsuario mu = ManejadorUsuario.getInstancia();
+        if(mu.existeUsuario(nickname)) {
+            //throw new UsuarioRepetidoException("El usuario " + ci + " ya esta registrado");
+        	return false;
+        }
+
+        Proveedor u = new Proveedor(nickname, nombre, apellido, correo,FNacimiento, descripcion, link);
+        mu.addUsuario(u);
+        return true;
+    }
+
 }
--- Implementacion/Servidor Central/src/logica/manejadores/ManejadorUsuario.java
+++ Implementacion/Servidor Central/src/logica/manejadores/ManejadorUsuario.java
@@ -1,8 +1,39 @@
 package logica.manejadores;
 
+import java.util.Map;
+
+import logica.entidades.Usuario;
+
 /**
  * @author Equipo taller prog 16
  */
 
 public class ManejadorUsuario {
+    private static ManejadorUsuario instancia;
+
+    private Map<String, Usuario> usuarios;   
+
+    public static ManejadorUsuario getInstancia(){
+        if(instancia == null){
+            instancia = new ManejadorUsuario();
+        }
+        return instancia;
+    }
+
+    public Map<String, Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void addUsuario(Usuario usuario) {
+        usuarios.put(usuario.getNombre(), usuario);
+    }
+
+    public Usuario getUsuario(String nombre) {
+        return usuarios.get(nombre);
+    }
+    
+    public Boolean existeUsuario(String nickname) {
+    	return usuarios.containsKey(nickname);
+    }
+    
 }
