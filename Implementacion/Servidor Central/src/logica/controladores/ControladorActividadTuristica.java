73782fe0a5e334b8fd649373ddec83235606b6ea
aguM-VM <tiqui800@gmail.com>
2022-08-20 02:00:51 -0300

    [Alta de Actividad] Primeros pasos

--- Implementacion/Servidor Central/src/logica/controladores/ControladorActividadTuristica.java
+++ Implementacion/Servidor Central/src/logica/controladores/ControladorActividadTuristica.java
@@ -1,8 +1,17 @@
 package logica.controladores;
 
+import java.util.Collection;
+
 /**
  * @author Equipo taller prog 16
  */
 
 public class ControladorActividadTuristica implements IControladorActividadTuristica {
+	public ControladorActividadTuristica() {
+	}
+	
+	public Collection<String> obtenerIdProveedores() {
+		ControladorUsuario CU;
+		return CU.obtenerIDProveedores();	
+	}
 }
