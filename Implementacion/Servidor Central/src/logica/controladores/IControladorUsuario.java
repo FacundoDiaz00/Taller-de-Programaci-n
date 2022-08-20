68c4ca25717df9b06b4f9f84851377d39efaa781
Valentina Alaniz <valentinaalaniz@gmail.com>
2022-08-20 03:41:33 -0300

    Mejoras en la ventana, no anda la operación de aceptar...

--- Implementacion/Servidor Central/src/logica/controladores/IControladorUsuario.java
+++ Implementacion/Servidor Central/src/logica/controladores/IControladorUsuario.java
@@ -2,6 +2,7 @@ package logica.controladores;
 
 import logica.datatypes.DTUsuario;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -12,4 +13,8 @@ public interface IControladorUsuario {
 	public List<String> obtenerIdUsuarios();
 
 	public DTUsuario obtenerDTUsuario(String nickname);
+	
+	public boolean altaTurista(String nickname, String nombre, String apellido, String correo,LocalDate FNacimiento, String nacionalidad);
+	
+	public boolean altaProveedor(String nickname,String nombre,String apellido,String correo,String descripcion,String link, LocalDate FNacimiento);
 }
