8ac007844a54637db02250e3e3b3b73243321891
agustin-recoba <agustin.recoba@fing.edu.uy>
2022-08-21 21:19:30 -0300

    Termino capa lógica

--- Implementacion/Servidor Central/src/logica/controladores/IControladorPaquete.java
+++ Implementacion/Servidor Central/src/logica/controladores/IControladorPaquete.java
@@ -1,5 +1,7 @@
 package logica.controladores;
 
+import java.util.List;
+
 /**
  * @author Equipo taller prog 16
  */
@@ -7,5 +9,13 @@ package logica.controladores;
 
 public interface IControladorPaquete {
     public boolean altaTurista(String nombre, String descripcion, int periodovalidez, float descuento);
+    
+    public List<String> obtenerIdPaquetes();
+    
+    public List<String> obtenerIdDepartamentos();
+    
+    public List<String> obtenerIdActividadesDeDepartamentoQueNoEstanEnPaquete(String nombreDep, String nombrePaq);
+    
+    public void agregarActividadAPaquete(String nombreAct, String nombrePaq);
 
 }
