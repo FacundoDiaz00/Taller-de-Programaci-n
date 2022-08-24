7b2c021d5dff6de3005f193f070e591a6c93f723
facundod <facundo.diaz.rodriguez@fing.edu.uy>
2022-08-24 18:54:08 -0300

    Commit inicial AltaDeSalidaTuristica

---
+++ Implementacion/Servidor Central/src/presentacion/AltaDeSalidaTuristica.java
@@ -0,0 +1,17 @@
+package presentacion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class AltaDeSalidaTuristica extends JInternalFrame {
+
+	/**
+	 * Create the frame.
+	 */
+	public AltaDeSalidaTuristica() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
