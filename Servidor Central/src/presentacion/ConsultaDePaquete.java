f38af071da8ad6a358f76681d24b15a3e941d3fb
facundod <facundo.diaz.rodriguez@fing.edu.uy>
2022-08-28 01:26:32 -0300

    Comienzo gui

---
+++ Servidor Central/src/presentacion/ConsultaDePaquete.java
@@ -0,0 +1,17 @@
+package presentacion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class ConsultaDePaquete extends JInternalFrame {
+
+	/**
+	 * Create the frame.
+	 */
+	public ConsultaDePaquete() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
