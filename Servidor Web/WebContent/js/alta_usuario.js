81d4f0ca596ecd28468f5547819293d31dccb5c0
Daniel Padron <daniel.padron@fing.edu.uy>
2022-09-15 00:02:19 -0300

    Quedo el cambio de eleccion en registrar usuario

---
+++ Servidor Web/WebContent/js/alta_usuario.js
@@ -0,0 +1,29 @@
+$("#checkTipoProveedor")[0].addEventListener('click', (e) => onProveedorClick(e))
+$("#checkTipoTurista")[0].addEventListener('click', (e) => onTuristaClick(e))
+
+
+const onTuristaClick = (e) =>{
+    console.log("Click en radiobutton turista")
+    $("#nacionalidad-field")[0].classList.remove("eliminarElemento")
+    $("#descripcion-general-field")[0].classList.add("eliminarElemento")
+    $("#link-sito-web-field")[0].classList.add("eliminarElemento")
+    $("#input-nacionalidad")[0].type = "text"
+    $("#input-descripcionGeneral")[0].type = "hidden"
+    $("#input-link")[0].type = "hidden"
+
+}
+
+const onProveedorClick = (e) =>{
+    console.log("Click en radiobutton turista")
+    $("#nacionalidad-field")[0].classList.add("eliminarElemento")
+    $("#descripcion-general-field")[0].classList.remove("eliminarElemento")
+    $("#link-sito-web-field")[0].classList.remove("eliminarElemento")
+
+    $("#input-nacionalidad")[0].type = "hidden"
+    $("#input-descripcionGeneral")[0].type = "text"
+    $("#input-link")[0].type = "text"
+}
+
+const onSubmit = () =>{
+
+}
