e6f0d212613246dbbb840d022ead3f3dc60ebbca
Unknown <facundo.diaz.rodriguez@fing.edu.uy>
2022-09-15 04:34:29 -0300

    Comienzo Js

---
+++ Servidor Web/WebContent/js/inscribirse_a_salida.js
@@ -0,0 +1,7 @@
+$("#checkGeneral")[0].addEventListener('click', (e) => onGeneralClick(e))
+$("#checkPaquete")[0].addEventListener('click', (e) => onPaqueteClick(e))
+
+
+const onGeneralClick = (e)=>{
+
+}
