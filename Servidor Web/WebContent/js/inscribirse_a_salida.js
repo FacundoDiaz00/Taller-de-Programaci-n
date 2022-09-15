25cb84da8b4c96d95674dd9ef228592f2d0f5b4e
Daniel Padron <daniel.padron@fing.edu.uy>
2022-09-15 17:52:36 -0300

    detalle

--- Servidor Web/WebContent/js/inscribirse_a_salida.js
+++ Servidor Web/WebContent/js/inscribirse_a_salida.js
@@ -3,15 +3,15 @@ $("#checkPaquete")[0].addEventListener('click', (e) => onPaqueteClick(e))
 
 
 const onGeneralClick = (e)=>{
-    console.log($("#comboPaquetes")[0].classLists)
+    console.log($("#comboPaquetes")[0].classList)
     $("#comboPaquetes")[0].classList.remove("eliminarElemento")
 /*
     ("#comboPaquetes")[0].classList.toggle("eliminarElemento")
     */
 }
 const onPaqueteClick = (e)=>{
-    console.log($("#comboPaquetes")[0].classLists)
-        $("#comboPaquetes")[0].classLists.add("eliminarElemento")
+    console.log($("#comboPaquetes")[0].classList)
+        $("#comboPaquetes")[0].classList.add("eliminarElemento")
     /*
     ("#comboPaquetes")[0].classList.toggle("eliminarElemento")
 */
