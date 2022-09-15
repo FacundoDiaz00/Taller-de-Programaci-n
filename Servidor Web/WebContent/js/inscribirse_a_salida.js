5bad5467f2102ea732d2820cffd98dcf2ae5a54f
Unknown <facundo.diaz.rodriguez@fing.edu.uy>
2022-09-15 17:47:00 -0300

    Se rompio internet

--- Servidor Web/WebContent/js/inscribirse_a_salida.js
+++ Servidor Web/WebContent/js/inscribirse_a_salida.js
@@ -3,5 +3,17 @@ $("#checkPaquete")[0].addEventListener('click', (e) => onPaqueteClick(e))
 
 
 const onGeneralClick = (e)=>{
-
+    console.log($("#comboPaquetes")[0].classLists)
+    $("#comboPaquetes")[0].classList.remove("eliminarElemento")
+/*
+    ("#comboPaquetes")[0].classList.toggle("eliminarElemento")
+    */
 }
+const onPaqueteClick = (e)=>{
+    console.log($("#comboPaquetes")[0].classLists)
+        $("#comboPaquetes")[0].classLists.add("eliminarElemento")
+    /*
+    ("#comboPaquetes")[0].classList.toggle("eliminarElemento")
+*/
+}
+
