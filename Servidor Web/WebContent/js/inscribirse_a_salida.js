e9cead2db46a72d4117dcbddcf8722f776556282
Unknown <facundo.diaz.rodriguez@fing.edu.uy>
2022-09-15 18:06:36 -0300

    Estaba al reves la seleccion y arreglo combobox

--- Servidor Web/WebContent/js/inscribirse_a_salida.js
+++ Servidor Web/WebContent/js/inscribirse_a_salida.js
@@ -4,14 +4,14 @@ $("#checkPaquete")[0].addEventListener('click', (e) => onPaqueteClick(e))
 
 const onGeneralClick = (e)=>{
     console.log($("#comboPaquetes")[0].classList)
-    $("#comboPaquetes")[0].classList.remove("eliminarElemento")
+    $("#comboPaquetes")[0].classList.add("eliminarElemento")
 /*
     ("#comboPaquetes")[0].classList.toggle("eliminarElemento")
     */
 }
 const onPaqueteClick = (e)=>{
     console.log($("#comboPaquetes")[0].classList)
-        $("#comboPaquetes")[0].classList.add("eliminarElemento")
+        $("#comboPaquetes")[0].classList.remove("eliminarElemento")
     /*
     ("#comboPaquetes")[0].classList.toggle("eliminarElemento")
 */
