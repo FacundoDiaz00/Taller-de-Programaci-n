d543a7ef47b1aeea4f861a6da55b2794a85e51a2
Daniel Padron <daniel.padron@fing.edu.uy>
2022-10-23 21:28:24 -0300

    Cambios en aparienza

--- Estacion de trabajo/src/presentacion/AltaDeActividadTuristica.java
+++ Estacion de trabajo/src/presentacion/AltaDeActividadTuristica.java
@@ -48,222 +48,222 @@ public class AltaDeActividadTuristica extends JInternalFrame {
     private DefaultListModel<String> listModelCategorias;
 
     /**
-     * Create the frame.
-     */
-    public AltaDeActividadTuristica(IControladorActividadTuristica contrAct) {
-        addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentShown(ComponentEvent event) {
-                actualizarListaCategorias();
-            }
-        });
-        Fabrica fabrica = Fabrica.getInstancia();
-        this.contrAct = fabrica.getIControladorActividadTuristica();
-        setTitle("Alta de Actividad Turística");
-        setBounds(100, 100, 600, 409);
-        getContentPane().setLayout(null);
-        setResizable(true);
-        setIconifiable(true);
-        setMaximizable(true);
-        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        setClosable(true);
-
-        JLabel lblNewLabel2 = new JLabel("Proveedores:");
-        lblNewLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel2.setBounds(7, 12, 120, 14);
-        getContentPane().add(lblNewLabel2);
-
-        JLabel lblNewLabel = new JLabel("Departamentos:");
-        lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel.setBounds(7, 37, 120, 14);
-        getContentPane().add(lblNewLabel);
-
-        JLabel lblNewLabel1 = new JLabel("Nombre:");
-        lblNewLabel1.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel1.setBounds(6, 63, 121, 14);
-        getContentPane().add(lblNewLabel1);
-
-        JLabel lblNewLabel11 = new JLabel("Descripción:");
-        lblNewLabel11.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel11.setBounds(7, 89, 120, 14);
-        getContentPane().add(lblNewLabel11);
-
-        JLabel lblNewLabel111 = new JLabel("Duración:");
-        lblNewLabel111.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel111.setBounds(7, 111, 120, 14);
-        getContentPane().add(lblNewLabel111);
-
-        JLabel lblNewLabel1111 = new JLabel("Costo:");
-        lblNewLabel1111.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel1111.setBounds(0, 139, 121, 14);
-        getContentPane().add(lblNewLabel1111);
-
-        JLabel lblNewLabel11111 = new JLabel("Ciudad:");
-        lblNewLabel11111.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel11111.setBounds(7, 165, 120, 14);
-        getContentPane().add(lblNewLabel11111);
-
-        JLabel lblNewLabel1111111 = new JLabel("Fecha de alta:");
-        lblNewLabel1111111.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblNewLabel1111111.setBounds(7, 191, 120, 14);
-        getContentPane().add(lblNewLabel1111111);
-
-        comboProveedores = new JComboBox<String>();
-        comboProveedores.setBounds(145, 7, 212, 24);
-        getContentPane().add(comboProveedores);
-
-        comboProveedores.addPopupMenuListener(new PopupMenuListener() {
-
-            @Override
-            public void popupMenuWillBecomeVisible(PopupMenuEvent event) {
-                // Esto es lo que actualiza la lista cada vez que se abre.
-                actualizarComboProveedores();
-            }
-
-            @Override
-            public void popupMenuWillBecomeInvisible(PopupMenuEvent event) {
-
-            }
-
-            @Override
-            public void popupMenuCanceled(PopupMenuEvent event) {
-
-            }
-
-        });
-
-        comboDepartamentos = new JComboBox();
-        comboDepartamentos.setBounds(145, 32, 212, 24);
-        getContentPane().add(comboDepartamentos);
-
-        nombre = new JTextField();
-        nombre.setBounds(131, 61, 258, 20);
-        getContentPane().add(nombre);
-        nombre.setColumns(10);
-
-        descripcion = new JTextField();
-        descripcion.setColumns(10);
-        descripcion.setBounds(131, 87, 258, 20);
-        getContentPane().add(descripcion);
-
-        duracion = new JTextField();
-        duracion.setColumns(10);
-        duracion.setBounds(131, 109, 258, 20);
-        getContentPane().add(duracion);
-
-        costo = new JTextField();
-        costo.setColumns(10);
-        costo.setBounds(131, 137, 258, 20);
-        getContentPane().add(costo);
-        // this.comboProveedores = comboProveedores;
-
-        comboDepartamentos.addPopupMenuListener(new PopupMenuListener() {
-
-            @Override
-            public void popupMenuWillBecomeVisible(PopupMenuEvent event) {
-                // Esto es lo que actualiza la lista cada vez que se abre.
-                actualizarComboDepartamentos();
-            }
-
-            @Override
-            public void popupMenuWillBecomeInvisible(PopupMenuEvent event) {
-
-            }
-
-            @Override
-            public void popupMenuCanceled(PopupMenuEvent event) {
-
-            }
-        });
-
-        JButton btnNewButton = new JButton("Aceptar");
-        btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                agregarAT(event);
-            }
-        });
-
-        ciudad = new JTextField();
-        ciudad.setColumns(10);
-        ciudad.setBounds(131, 163, 258, 20);
-        getContentPane().add(ciudad);
-        ciudad.setEnabled(true);
-
-        fDeAlta = new JTextField();
-        fDeAlta.setColumns(10);
-        fDeAlta.setBounds(131, 189, 258, 20);
-        getContentPane().add(fDeAlta);
-        btnNewButton.setBounds(270, 340, 117, 25);
-        getContentPane().add(btnNewButton);
-
-        JButton btnCancelar = new JButton("Cancelar");
-        btnCancelar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                limpiarFormulario();
-                setVisible(false);
-            }
-        });
-        btnCancelar.setBounds(7, 340, 117, 25);
-        getContentPane().add(btnCancelar);
-
-        JLabel lblCategorias = new JLabel("Categorías");
-        lblCategorias.setHorizontalAlignment(SwingConstants.CENTER);
-        lblCategorias.setBounds(7, 217, 151, 15);
-        getContentPane().add(lblCategorias);
-
-        JList listCategorias = new JList();
-        listCategorias.setBounds(12, 233, 139, 95);
-        getContentPane().add(listCategorias);
-        categoriasList = listCategorias;
-
-        JLabel lblSeleccion = new JLabel("Selección");
-        lblSeleccion.setHorizontalAlignment(SwingConstants.CENTER);
-        lblSeleccion.setBounds(250, 217, 137, 15);
-        getContentPane().add(lblSeleccion);
-
-        JList listSeleccion = new JList();
-        listSeleccion.setBounds(250, 233, 139, 95);
-        getContentPane().add(listSeleccion);
-        seleccionList = listSeleccion;
-        seleccionList.setModel(listModelSeleccion);
-
-        JButton btnDerecha = new JButton(">");
-        btnDerecha.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String elem = categoriasList.getSelectedValue();
-                if (elem != null) {
-                    listModelSeleccion.addElement(elem);
-                    listModelCategorias.removeElement(elem);
-                }
-
-            }
-        });
-        btnDerecha.setBounds(171, 233, 59, 25);
-        getContentPane().add(btnDerecha);
-
-        JButton btnIzquierda = new JButton("<");
-        btnIzquierda.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String elem = seleccionList.getSelectedValue();
-                if (elem != null) {
-                    listModelCategorias.addElement(elem);
-                    listModelSeleccion.removeElement(elem);
-                }
-            }
-        });
-        btnIzquierda.setBounds(171, 303, 59, 25);
-        getContentPane().add(btnIzquierda);
-
-        JButton btnClear = new JButton("CLR");
-        btnClear.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent eevent) {
-                listModelSeleccion.clear();
-                actualizarListaCategorias();
-            }
-        });
-        btnClear.setBounds(163, 266, 75, 25);
-        getContentPane().add(btnClear);
-    }
+	 * Create the frame.
+	 */
+	public AltaDeActividadTuristica(IControladorActividadTuristica contrAct) {
+	    addComponentListener(new ComponentAdapter() {
+	        @Override
+	        public void componentShown(ComponentEvent event) {
+	            actualizarListaCategorias();
+	        }
+	    });
+	    Fabrica fabrica = Fabrica.getInstancia();
+	    this.contrAct = fabrica.getIControladorActividadTuristica();
+	    setTitle("Alta de Actividad Turística");
+	    setBounds(100, 100, 492, 409);
+	    getContentPane().setLayout(null);
+	    setResizable(true);
+	    setIconifiable(true);
+	    setMaximizable(true);
+	    setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+	    setClosable(true);
+	
+	    JLabel lblNewLabel2 = new JLabel("Proveedores:");
+	    lblNewLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel2.setBounds(49, 16, 70, 14);
+	    getContentPane().add(lblNewLabel2);
+	
+	    JLabel lblNewLabel = new JLabel("Departamentos:");
+	    lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel.setBounds(30, 41, 89, 14);
+	    getContentPane().add(lblNewLabel);
+	
+	    JLabel lblNewLabel1 = new JLabel("Nombre:");
+	    lblNewLabel1.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel1.setBounds(48, 66, 71, 14);
+	    getContentPane().add(lblNewLabel1);
+	
+	    JLabel lblNewLabel11 = new JLabel("Descripción:");
+	    lblNewLabel11.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel11.setBounds(49, 91, 70, 14);
+	    getContentPane().add(lblNewLabel11);
+	
+	    JLabel lblNewLabel111 = new JLabel("Duración:");
+	    lblNewLabel111.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel111.setBounds(49, 114, 70, 14);
+	    getContentPane().add(lblNewLabel111);
+	
+	    JLabel lblNewLabel1111 = new JLabel("Costo:");
+	    lblNewLabel1111.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel1111.setBounds(42, 139, 77, 14);
+	    getContentPane().add(lblNewLabel1111);
+	
+	    JLabel lblNewLabel11111 = new JLabel("Ciudad:");
+	    lblNewLabel11111.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel11111.setBounds(74, 167, 45, 14);
+	    getContentPane().add(lblNewLabel11111);
+	
+	    JLabel lblNewLabel1111111 = new JLabel("Fecha de alta:");
+	    lblNewLabel1111111.setHorizontalAlignment(SwingConstants.RIGHT);
+	    lblNewLabel1111111.setBounds(37, 192, 82, 14);
+	    getContentPane().add(lblNewLabel1111111);
+	
+	    comboProveedores = new JComboBox<String>();
+	    comboProveedores.setBounds(123, 10, 334, 24);
+	    getContentPane().add(comboProveedores);
+	
+	    comboProveedores.addPopupMenuListener(new PopupMenuListener() {
+	
+	        @Override
+	        public void popupMenuWillBecomeVisible(PopupMenuEvent event) {
+	            // Esto es lo que actualiza la lista cada vez que se abre.
+	            actualizarComboProveedores();
+	        }
+	
+	        @Override
+	        public void popupMenuWillBecomeInvisible(PopupMenuEvent event) {
+	
+	        }
+	
+	        @Override
+	        public void popupMenuCanceled(PopupMenuEvent event) {
+	
+	        }
+	
+	    });
+	
+	    comboDepartamentos = new JComboBox();
+	    comboDepartamentos.setBounds(123, 35, 334, 24);
+	    getContentPane().add(comboDepartamentos);
+	
+	    nombre = new JTextField();
+	    nombre.setBounds(123, 63, 334, 20);
+	    getContentPane().add(nombre);
+	    nombre.setColumns(10);
+	
+	    descripcion = new JTextField();
+	    descripcion.setColumns(10);
+	    descripcion.setBounds(123, 89, 334, 20);
+	    getContentPane().add(descripcion);
+	
+	    duracion = new JTextField();
+	    duracion.setColumns(10);
+	    duracion.setBounds(123, 111, 334, 20);
+	    getContentPane().add(duracion);
+	
+	    costo = new JTextField();
+	    costo.setColumns(10);
+	    costo.setBounds(123, 135, 334, 20);
+	    getContentPane().add(costo);
+	    // this.comboProveedores = comboProveedores;
+	
+	    comboDepartamentos.addPopupMenuListener(new PopupMenuListener() {
+	
+	        @Override
+	        public void popupMenuWillBecomeVisible(PopupMenuEvent event) {
+	            // Esto es lo que actualiza la lista cada vez que se abre.
+	            actualizarComboDepartamentos();
+	        }
+	
+	        @Override
+	        public void popupMenuWillBecomeInvisible(PopupMenuEvent event) {
+	
+	        }
+	
+	        @Override
+	        public void popupMenuCanceled(PopupMenuEvent event) {
+	
+	        }
+	    });
+	
+	    JButton btnNewButton = new JButton("Aceptar");
+	    btnNewButton.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent event) {
+	            agregarAT(event);
+	        }
+	    });
+	
+	    ciudad = new JTextField();
+	    ciudad.setColumns(10);
+	    ciudad.setBounds(123, 161, 334, 20);
+	    getContentPane().add(ciudad);
+	    ciudad.setEnabled(true);
+	
+	    fDeAlta = new JTextField();
+	    fDeAlta.setColumns(10);
+	    fDeAlta.setBounds(123, 189, 334, 20);
+	    getContentPane().add(fDeAlta);
+	    btnNewButton.setBounds(340, 340, 117, 25);
+	    getContentPane().add(btnNewButton);
+	
+	    JButton btnCancelar = new JButton("Cancelar");
+	    btnCancelar.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent event) {
+	            limpiarFormulario();
+	            setVisible(false);
+	        }
+	    });
+	    btnCancelar.setBounds(7, 340, 117, 25);
+	    getContentPane().add(btnCancelar);
+	
+	    JLabel lblCategorias = new JLabel("Categorías");
+	    lblCategorias.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblCategorias.setBounds(7, 216, 75, 15);
+	    getContentPane().add(lblCategorias);
+	
+	    JList listCategorias = new JList();
+	    listCategorias.setBounds(12, 233, 183, 95);
+	    getContentPane().add(listCategorias);
+	    categoriasList = listCategorias;
+	
+	    JLabel lblSeleccion = new JLabel("Selección");
+	    lblSeleccion.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblSeleccion.setBounds(269, 220, 75, 15);
+	    getContentPane().add(lblSeleccion);
+	
+	    JList listSeleccion = new JList();
+	    listSeleccion.setBounds(274, 234, 183, 95);
+	    getContentPane().add(listSeleccion);
+	    seleccionList = listSeleccion;
+	    seleccionList.setModel(listModelSeleccion);
+	
+	    JButton btnDerecha = new JButton(">");
+	    btnDerecha.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent event) {
+	            String elem = categoriasList.getSelectedValue();
+	            if (elem != null) {
+	                listModelSeleccion.addElement(elem);
+	                listModelCategorias.removeElement(elem);
+	            }
+	
+	        }
+	    });
+	    btnDerecha.setBounds(205, 229, 59, 25);
+	    getContentPane().add(btnDerecha);
+	
+	    JButton btnIzquierda = new JButton("<");
+	    btnIzquierda.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent event) {
+	            String elem = seleccionList.getSelectedValue();
+	            if (elem != null) {
+	                listModelCategorias.addElement(elem);
+	                listModelSeleccion.removeElement(elem);
+	            }
+	        }
+	    });
+	    btnIzquierda.setBounds(205, 303, 59, 25);
+	    getContentPane().add(btnIzquierda);
+	
+	    JButton btnClear = new JButton("CLR");
+	    btnClear.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent eevent) {
+	            listModelSeleccion.clear();
+	            actualizarListaCategorias();
+	        }
+	    });
+	    btnClear.setBounds(205, 265, 59, 25);
+	    getContentPane().add(btnClear);
+	}
 
     private void agregarAT(ActionEvent action) {
 
