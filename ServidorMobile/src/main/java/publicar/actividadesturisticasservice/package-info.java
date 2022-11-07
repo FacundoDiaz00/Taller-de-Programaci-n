9b99a5534a5fd2164a9c4a1df5e44bdac7af0ddc
Valentina Alaniz <natalie.alaniz@fing.edu.uy>
2022-11-06 21:20:45 -0300

    borre actividad tusitica en publicar

--- ServidorMobile/src/main/java/publicar/actividadesturisticasservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://actividadesTuristicasService.publicar/")
-package publicar.actividadesturisticasservice;
--- ServidorMobile/src/main/java/publicar/maestroservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://maestroServices.publicar/")
-package publicar.maestroservices;
