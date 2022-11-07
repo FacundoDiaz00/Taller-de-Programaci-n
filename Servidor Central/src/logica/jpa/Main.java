8325812a066765e1ed608ca8aec104f66f94c0ac
Agustín Recoba <agustin.recoba@fing.edu.uy>
2022-11-07 02:07:53 -0300

    Sigo ahora con eclipse

--- Servidor Central/src/logica/jpa/Main.java
+++ Servidor Central/src/logica/jpa/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ActividadJPA act = new ActividadJPA();
-        act.setNombre("aba");
+        act.setNombre("abija");
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
