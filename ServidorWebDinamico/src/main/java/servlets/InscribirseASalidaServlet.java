168537f5006b810b0998552411290a4ed919f72c
agustin-recoba <agustin.recoba@fing.edu.uy>
2022-11-14 17:20:28 -0300

    Redirects erroneos

--- ServidorWebDinamico/src/main/java/servlets/InscribirseASalidaServlet.java
+++ ServidorWebDinamico/src/main/java/servlets/InscribirseASalidaServlet.java
@@ -1,7 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -23,7 +22,6 @@ import publicar.actividadesturisticasservice.SuperaElMaximoDeTuristasException_E
 import publicar.actividadesturisticasservice.WebServiceActividades;
 import publicar.actividadesturisticasservice.WebServiceActividadesService;
 import publicar.usuarioturisticasservice.DtTurista;
-import publicar.usuarioturisticasservice.DtUsuario;
 import utils.Utile;
 
 /**
@@ -31,129 +29,132 @@ import utils.Utile;
  */
 @WebServlet("/InscribiseASalida")
 public class InscribirseASalidaServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private WebServiceActividades wbActi;
-    private DtSalidaTuristica salida = null;
+	private static final long serialVersionUID = 1L;
+	private WebServiceActividades wbActi;
+	private DtSalidaTuristica salida = null;
 
-    public InscribirseASalidaServlet() {
-        super();
-        wbActi = new WebServiceActividadesService().getWebServiceActividadesPort();
-    }
+	public InscribirseASalidaServlet() {
+		super();
+		wbActi = new WebServiceActividadesService().getWebServiceActividadesPort();
+	}
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getCharacterEncoding() == null) {
-            req.setCharacterEncoding("UTF-8");
-        }
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if (req.getCharacterEncoding() == null) {
+			req.setCharacterEncoding("UTF-8");
+		}
 
-        String nombSalida = (String) req.getParameter("id");
+		String nombSalida = (String) req.getParameter("id");
 
-        if (nombSalida == null) {
-            req.setAttribute("motivoDeError", "No se ha incluido el id de la salida en los parámetros");
-            req.getRequestDispatcher("/WEB-INF/jsp/consulta_de_salida.jsp").forward(req, resp);
-        }
+		if (nombSalida == null) {
+			req.setAttribute("motivoDeError", "No se ha incluido el id de la salida en los parámetros");
+			req.getRequestDispatcher("/WEB-INF/jsp/consulta_de_salida_turistica.jsp").forward(req, resp);
+		}
 
-        try {
+		try {
 
-            salida = wbActi.obtenerDTSalidaTuristica(nombSalida);
-            req.setAttribute("salida", salida);
-            req = Utile.insertarLoDeSiempre(req);
-            DtTurista turi = (DtTurista) req.getSession().getAttribute("usuarioLogeado");
-            String nickTuri = "";
-            if (turi != null) {
-                nickTuri = turi.getNickname();
-            } else {
-                req = Utile.insertarLoDeSiempre(req);
-                resp.sendRedirect("index");
-                return;
-            }
+			salida = wbActi.obtenerDTSalidaTuristica(nombSalida);
+			req.setAttribute("salida", salida);
+			req = Utile.insertarLoDeSiempre(req);
+			DtTurista turi = (DtTurista) req.getSession().getAttribute("usuarioLogeado");
+			String nickTuri = "";
+			if (turi != null) {
+				nickTuri = turi.getNickname();
+			} else {
+				req = Utile.insertarLoDeSiempre(req);
+				resp.sendRedirect("index");
+				return;
+			}
 
-            List<String> paquetes = wbActi.obtenerIdComprasDisponiblesParaInscripcion(salida.getActividad(),
-                    nickTuri).getItem();
-            req.setAttribute("paquetes", paquetes);
+			List<String> paquetes = wbActi.obtenerIdComprasDisponiblesParaInscripcion(salida.getActividad(), nickTuri)
+					.getItem();
+			req.setAttribute("paquetes", paquetes);
 
-        } catch (ObjetoNoExisteEnTurismoUy_Exception e) {
-            req.setAttribute("motivoDeError", "No existe la salida turistica");
-            req.getRequestDispatcher("/WEB-INF/jsp/consulta_de_salida.jsp").forward(req, resp);
-            return;
-        }
+		} catch (ObjetoNoExisteEnTurismoUy_Exception e) {
+			req.setAttribute("motivoDeError", "No existe la salida turistica");
+			req.getRequestDispatcher("/WEB-INF/jsp/errores/400.jsp").forward(req, resp);
+			return;
+		} catch (ClassCastException e) {
+			req.setAttribute("motivoDeError", "El usuario logueado no es proveedor");
+			req.getRequestDispatcher("/WEB-INF/jsp/errores/400.jsp").forward(req, resp);
+			return;
+		}
 
-        req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-    }
+		req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+	}
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getCharacterEncoding() == null) {
-            req.setCharacterEncoding("UTF-8");
-        }
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if (req.getCharacterEncoding() == null) {
+			req.setCharacterEncoding("UTF-8");
+		}
 
-        String formaDePago = (String) req.getParameter("formaPago");
-        int cantTuristas = Integer.parseInt(req.getParameter("cantTuristas"));
-        DtTurista turi = (DtTurista) req.getSession().getAttribute("usuarioLogeado");
-        String nickTuri = "";
-        if (turi != null && turi instanceof DtTurista) {
-            nickTuri = turi.getNickname();
-        } else {
-            req = Utile.insertarLoDeSiempre(req);
-            resp.sendRedirect("index");
-            return;
-        }
+		String formaDePago = (String) req.getParameter("formaPago");
+		int cantTuristas = Integer.parseInt(req.getParameter("cantTuristas"));
+		DtTurista turi = (DtTurista) req.getSession().getAttribute("usuarioLogeado");
+		String nickTuri = "";
+		if (turi != null && turi instanceof DtTurista) {
+			nickTuri = turi.getNickname();
+		} else {
+			req = Utile.insertarLoDeSiempre(req);
+			resp.sendRedirect("index");
+			return;
+		}
 
-        String nombrePaquete = "";
-        if (formaDePago.equals("1")) {
-            nombrePaquete = (String) req.getParameter("paquete");
-        }
+		String nombrePaquete = "";
+		if (formaDePago.equals("1")) {
+			nombrePaquete = (String) req.getParameter("paquete");
+		}
 
-        try {
-            List<String> paquetes = wbActi.obtenerIdComprasDisponiblesParaInscripcion(salida.getActividad(),
-                    nickTuri).getItem();
-            req.setAttribute("paquetes", paquetes);
-        } catch (ObjetoNoExisteEnTurismoUy_Exception e) {
-            req.setAttribute("motivoDeError", "El paquete ingresado para la inscripción no existe");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        }
+		try {
+			List<String> paquetes = wbActi.obtenerIdComprasDisponiblesParaInscripcion(salida.getActividad(), nickTuri)
+					.getItem();
+			req.setAttribute("paquetes", paquetes);
+		} catch (ObjetoNoExisteEnTurismoUy_Exception e) {
+			req.setAttribute("motivoDeError", "El paquete ingresado para la inscripción no existe");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		}
 
-        req = Utile.insertarLoDeSiempre(req);
-        req.setAttribute("salida", this.salida);
+		req = Utile.insertarLoDeSiempre(req);
+		req.setAttribute("salida", this.salida);
 
-        try {
-        	wbActi.altaInscripcionSalidaTuristica(salida.getNombre(), nickTuri, cantTuristas,
-                    nombrePaquete);
+		try {
+			wbActi.altaInscripcionSalidaTuristica(salida.getNombre(), nickTuri, cantTuristas, nombrePaquete);
 
-            req.setAttribute("exito", Boolean.TRUE);
-            req = Utile.insertarLoDeSiempre(req);
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (InscripcionYaRegistradaException_Exception e) {
-            req.setAttribute("motivoDeError", "Usuario ya inscripto para esta salida turística");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (SuperaElMaximoDeTuristasException_Exception e) {
-            req.setAttribute("motivoDeError", "La cantidad de turistas ingresadas supera el máximo");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (FechaAltaSalidaTuristicaPosteriorAFechaInscripcion_Exception e) {
-            req.setAttribute("motivoDeError",
-                    "La fecha de la salida turística no puede ser posterior a la fecha de inscripción");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (AltaInscripcionPosteriorAFechaSalidaException_Exception e) {
-            req.setAttribute("motivoDeError",
-                    "La fecha de la inscripción no puede ser posterior a la fecha de la salida asociada");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (PaqueteNoCompradoExcepcion_Exception e) {
-            req.setAttribute("motivoDeError", "Se debe comprar el paquete para poder inscribirse a la salida asociada");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (CompraPaqueteVencidoExcepcion_Exception e) {
-            req.setAttribute("motivoDeError", "La compra asociada a la salida turistica se encuentra vencida");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (CompraConConsumosInsuficientesExcepcion_Exception e) {
-            req.setAttribute("motivoDeError",
-                    "La compra asociada a la salida turisitica se encuentra sin consumos disponibles");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (NoExisteConsumoParaLaActividadExcepcion_Exception e) {
-            req.setAttribute("motivoDeError",
-                    "No existe ningún consumo para la actividad asociada a la salida seleccionada");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        } catch (ObjetoNoExisteEnTurismoUy_Exception e) {
-            req.setAttribute("motivoDeError", "No existe la salida a la que se desea inscribir");
-            req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
-        }
+			req.setAttribute("exito", Boolean.TRUE);
+			req = Utile.insertarLoDeSiempre(req);
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (InscripcionYaRegistradaException_Exception e) {
+			req.setAttribute("motivoDeError", "Usuario ya inscripto para esta salida turística");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (SuperaElMaximoDeTuristasException_Exception e) {
+			req.setAttribute("motivoDeError", "La cantidad de turistas ingresadas supera el máximo");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (FechaAltaSalidaTuristicaPosteriorAFechaInscripcion_Exception e) {
+			req.setAttribute("motivoDeError",
+					"La fecha de la salida turística no puede ser posterior a la fecha de inscripción");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (AltaInscripcionPosteriorAFechaSalidaException_Exception e) {
+			req.setAttribute("motivoDeError",
+					"La fecha de la inscripción no puede ser posterior a la fecha de la salida asociada");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (PaqueteNoCompradoExcepcion_Exception e) {
+			req.setAttribute("motivoDeError", "Se debe comprar el paquete para poder inscribirse a la salida asociada");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (CompraPaqueteVencidoExcepcion_Exception e) {
+			req.setAttribute("motivoDeError", "La compra asociada a la salida turistica se encuentra vencida");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (CompraConConsumosInsuficientesExcepcion_Exception e) {
+			req.setAttribute("motivoDeError",
+					"La compra asociada a la salida turisitica se encuentra sin consumos disponibles");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (NoExisteConsumoParaLaActividadExcepcion_Exception e) {
+			req.setAttribute("motivoDeError",
+					"No existe ningún consumo para la actividad asociada a la salida seleccionada");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		} catch (ObjetoNoExisteEnTurismoUy_Exception e) {
+			req.setAttribute("motivoDeError", "No existe la salida a la que se desea inscribir");
+			req.getRequestDispatcher("/WEB-INF/jsp/inscribirse_a_salida.jsp").forward(req, resp);
+		}
 
-    }
+	}
 
 }
